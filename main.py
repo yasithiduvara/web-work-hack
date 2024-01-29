40691e1647ef8555ea07b68354ec7c7e125178ef
Induvara Yasith <induvaray@codegen.net>
2024-01-29 09:40:26 +0530

    time 5min key shift

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hello")
+import pyautogui
+import time
+pyautogui.FAILSAFE = False
+
+
+while True:
+    time.sleep(120)
+    for i in range(10, 100):
+        pyautogui.moveTo(500, i * 5)
+    for i in range(0, 2):
+        pyautogui.press('shift')
+        # pyautogui.typewrite(' ')
+
